bb3a6d5431bf888de8d3ff79f55fd719a33a7b7a
Anton <tepa-86@mail.ru>
2023-11-19 20:25:45 +0300

    Редактирование файла schaefermarine_parse.py:
    - Добавление функции getting_category_page_links:
    
      Поиск и добавление ссылок на все страницы категорий продуктов,
      для дальнейшего извлечения ссылок на продукты

--- schaefermarine_parse.py
+++ schaefermarine_parse.py
@@ -37,19 +37,43 @@ def getting_product_category_links(soup: BeautifulSoup, url: str) -> list:
     return list_category_links
 
 
+def getting_category_page_links(base_url: str, headers: dict, category_links: list) -> list:
+    links_to_category_pages = []
+
+    for link_to_category in category_links:
+        url_pages = link_to_category
+        links_to_category_pages.append(url_pages)
+
+        while True:
+            soup = fetch_html_content(url_pages, headers)
+
+            next_button = soup.find('span', class_='next')
+
+            if next_button is None:
+                break
+
+            link_next_button = next_button.find('a')['href']
+            url_pages = f'{base_url.rstrip("/")}{link_next_button}'
+
+            links_to_category_pages.append(url_pages)
+
+    return links_to_category_pages
+
+
 def main():
-    url = 'https://hardware.schaefermarine.com/'
+    base_url = 'https://hardware.schaefermarine.com/'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     }
 
-    soup = fetch_html_content(url, headers)
-    all_product_category_links = getting_product_category_links(soup, url)
+    soup = fetch_html_content(base_url, headers)
+    all_product_category_links = getting_product_category_links(soup, base_url)
+    all_links_to_category_pages = getting_category_page_links(base_url, headers, all_product_category_links)
 
     count = 1
-    for product in all_product_category_links:
-        print(f'{count}. {product}')
+    for i in all_links_to_category_pages:
+        print(f'{count}. {i}')
         count += 1
 
 
